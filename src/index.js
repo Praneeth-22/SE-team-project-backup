de06b3e7ed653bc06b14c3e566db270d13ec040a
Ajay kumar Boreddy <ajaykumarboreddy@dyn-169-226-246-245.wireless.albany.edu>
2023-03-07 13:51:44 -0500

    chats components added

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Chat from './Components/Chat/Chatpage';
+import Register from './Components/Chat/Register';
+import Home from './Components/Chat/Home';
+import { AuthContextProvider } from './Components/Chat/AuthContext';
+import { ChatContextProvider } from './Components/Chat/ChatContext';
+import Routing from './Components/Chat/Routing';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Chat />
+<AuthContextProvider>
+<ChatContextProvider>
+<React.StrictMode>
+    <Routing/>
   </React.StrictMode>
+  </ChatContextProvider>
+</AuthContextProvider>
+  
 );
 
